7fdd802bbb31abe1105adb1819a4574b14df8f49
Kim, JeongHoon <jeonghoon.kim@rakuten.com>
2017-03-10 17:53:13 +0800

    reverted back

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-
 import App from './App';
 import './index.css';
-
-/*
- *  Action
- */
-const INCREMENT = "INCREMENT";
-
-function increase(diff) {
-  return {
-    type: INCREMENT,
-    addBy: diff
-  };
-}
-
-/*
- *  Reducer
- */
-const initialState = {
-  value: 0
-};
-
-const counterReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case INCREMENT:
-      return Object.assign({}, state, {
-        value: state.value + action.addBy
-      });
-    default:
-      return state;
-  }
-}
-
-/*
- *  Store
- */
-const store = createStore(counterReducer);
 
 ReactDOM.render(
   <App />,
